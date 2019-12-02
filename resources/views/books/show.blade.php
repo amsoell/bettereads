8857460e473ee3bbdca1260a62d76e852c0d72ea
Andy Soell <andy@teamsoell.com>
2019-12-02 16:59:52 +0100

    update formatting on book show view

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -5,14 +5,32 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Book') }}</div>
+                <div class="card-header">{{ $book->title}}</div>
 
-                <div class="card-body">
-                    <h1>{{ $book->title}}</h1>
-                    @isset ($book->subtitle)
-                    <h2>{{ $book->subtitle }}</h2>
-                    @endisset
-                    <img src="{{ $book->cover->medium }}" />
+                <div class="card-body row">
+                    <div class="col-xs-12 col-md-4">
+                        <img src="{{ $book->cover->medium }}" />
+                    </div>
+                    <div class="col-xs-12 col-md-8">
+                        @isset ($book->subtitle)
+                        <h2>{{ $book->subtitle }}</h2>
+                        @endisset
+                        <dl>
+                            <dt>{{ Str::plural('Author', count($book->authors)) }}</dt>
+                            <dd>
+                            @foreach ($book->authors as $author)
+                                {{ $author->name }}
+                                @unless ($loop->last)
+                                <br />
+                                @endunless
+                            @endforeach
+                            </dd>
+                            <dt>Pages:</dt>
+                            <dd>{{ $book->number_of_pages }}</dd>
+                            <dt>Published:</dt>
+                            <dd>{{ $book->publish_date }}</dd>
+                        </dl>
+                    </div>
                 </div>
             </div>
         </div>
